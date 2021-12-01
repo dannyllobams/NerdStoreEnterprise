c314c9073273124f60e8499f1fde9320e85137ac
Dannyllo Baltazar <dannyllo.souza@tsea.com.br>
2021-12-01 00:40:26 -0300

    Consumindo uma API via HTTP Services

--- src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,29 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NSE.WebApp.MVC.Models;
+using NSE.WebApp.MVC.Services;
 using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
-        [HttpGet]
-        [Route("nova-conta")]
-        public IActionResult Registro()
+        private readonly IAutenticacaoService _autenticacaoService;
+
+        public IdentidadeController(IAutenticacaoService autenticacaoService)
         {
-            return View();
-        }
-
-        [HttpPost]
-        [Route("nova-conta")]
-        public async Task<IActionResult> Registro(UsuarioRegistro model)
-        {
-            if (!ModelState.IsValid) 
-                return View(model);
-
-            if (false) 
-                return View(model);
-
-            return RedirectToAction("Index", "Home");
+            _autenticacaoService = autenticacaoService;
         }
 
         [HttpGet()]
@@ -40,7 +28,31 @@ namespace NSE.WebApp.MVC.Controllers
             if (!ModelState.IsValid) 
                 return View(model);
 
+            var resposta = _autenticacaoService.Login(model); 
+
             if (false) 
+                return View(model);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var resposta = _autenticacaoService.Registro(model);
+
+            if (false)
                 return View(model);
 
             return RedirectToAction("Index", "Home");
