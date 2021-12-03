c5845d92c17e24ace76273f5511ffb5b4426088a
Dannyllo Baltazar <dannyllo.souza@tsea.com.br>
2021-12-02 23:55:45 -0300

    Obtendo dados de usuário do cookie

---
+++ src/web/NSE.WebApp.MVC/Extensions/IUser.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public interface IUser
+    {        
+        string Nome { get; }
+        Guid ObterUserId();
+        string ObterUserEmail();
+        string ObterUserToken();
+        bool EstaAutenticado();
+        bool PossuiRole(string role);
+        IEnumerable<Claim> ObterClaims();
+        HttpContext ObterHttpContext();
+    }
+
+    public class AspNetUser : IUser
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public AspNetUser(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return _contextAccessor.HttpContext.User.Identity.Name;
+            }
+        }
+
+        public Guid ObterUserId()
+        {
+            return this.EstaAutenticado() ? Guid.Parse(_contextAccessor.HttpContext.User.GetUserId()) : Guid.Empty;
+        }
+        public string ObterUserEmail()
+        {
+            return this.EstaAutenticado() ? _contextAccessor.HttpContext.User.GetUserEmail() : null;
+        }
+
+        public string ObterUserToken()
+        {
+            return this.EstaAutenticado() ? _contextAccessor.HttpContext.User.GetUserToken() : null;
+        }
+
+        public bool EstaAutenticado()
+        {
+            return _contextAccessor.HttpContext.User.Identity.IsAuthenticated;
+        }
+
+        public IEnumerable<Claim> ObterClaims()
+        {
+            return _contextAccessor.HttpContext.User.Claims;
+        }
+
+        public HttpContext ObterHttpContext()
+        {
+            return _contextAccessor.HttpContext;
+        }
+
+        public bool PossuiRole(string role)
+        {
+            return _contextAccessor.HttpContext.User.IsInRole(role);
+        }
+    }
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal principal)
+        {
+            if(principal == null)
+            {
+                throw new ArgumentException(nameof(principal));
+            }
+
+            var claim = principal.FindFirst("sub");
+            return claim?.Value;
+        }
+
+        public static string GetUserEmail(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentException(nameof(principal));
+            }
+
+            var claim = principal.FindFirst("email");
+            return claim?.Value;
+        }
+
+        public static string GetUserToken(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentException(nameof(principal));
+            }
+
+            var claim = principal.FindFirst("JWT");
+            return claim?.Value;
+        }
+    }
+}
