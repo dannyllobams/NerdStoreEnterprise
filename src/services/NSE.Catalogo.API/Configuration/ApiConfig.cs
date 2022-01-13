6e78422008245eaf5bedd3b8b6b3521762396d32
Dannyllo Baltazar <dannyllobams@gmail.com>
2022-01-13 01:11:03 -0300

    Validação de JWT na API Catalogo

--- src/services/NSE.Catalogo.API/Configuration/ApiConfig.cs
+++ src/services/NSE.Catalogo.API/Configuration/ApiConfig.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NSE.Catalogo.API.Data;
+using NSE.WebAPI.Core.Identidade;
 
 namespace NSE.Catalogo.API.Configuration
 {
@@ -37,6 +38,7 @@ namespace NSE.Catalogo.API.Configuration
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseCors("Total");
+            app.UseAuthConfiguration();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
