b37fe47c5b61e9f117be6dc89b882d5f850da025
Dannyllo Baltazar <dannyllobams@gmail.com>
2022-05-02 19:45:48 -0300

    Catalogo HttpService

---
+++ src/web/NSE.WebApp.MVC/Services/ICatalogoService.cs
@@ -0,0 +1,13 @@
+﻿using NSE.WebApp.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Services
+{
+    public interface ICatalogoService
+    {
+        Task<ProdutoViewModel> ObterPorId(Guid id);
+        Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+    }
+}
